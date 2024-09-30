44e7135b2b8703bfd2e15b774fa86543e5e1bf4e
Denis Kazakov <dykazakov@avito.ru>
2024-09-30 22:59:20 +0400

    rewrite shutdown

--- aiofutures/executor.py
+++ aiofutures/executor.py
@@ -35,28 +35,38 @@ Output:
     result 2: ((2,), {'b': 2})
 """
 import asyncio
+import time
 from concurrent.futures import Executor, Future, ThreadPoolExecutor
 from threading import Thread
-from typing import Awaitable, Callable, Optional
+from typing import Awaitable, Callable, Optional, Set
 
 from .exceptions import InvalidStateError
 
 
 class AsyncExecutor(Executor):
     """The executor that runs coroutines in a different thread."""
+    _loop: asyncio.BaseEventLoop
 
     def __init__(self, executor: Optional[ThreadPoolExecutor] = None) -> None:
         def run():
+            self._loop = asyncio.new_event_loop()
+            if executor:
+                self._loop.set_default_executor(executor)
+
             asyncio.set_event_loop(self._loop)
             self._loop.run_forever()
 
         self._stopped = False
-        self._loop = asyncio.new_event_loop()
-        if executor:
-            self._loop.set_default_executor(executor)
-
         self._thread = Thread(target=run, name=self.__class__.__name__, daemon=True)
         self._thread.start()
+
+    def __repr__(self) -> str:
+        cls = self.__class__.__name__
+        return f'<{cls} stopped={self._stopped} tasks={len(self.tasks)}>'
+
+    @property
+    def tasks(self) -> Set[asyncio.Task]:
+        return asyncio.all_tasks(self._loop)
 
     def submit(self, task: Callable[..., Awaitable], *args, **kwargs) -> Future:  # type: ignore[override]
         """Schedule new async task.
@@ -77,7 +87,7 @@ class AsyncExecutor(Executor):
         """
         if self._stopped:
             raise InvalidStateError(f'The task was submitted after AsyncExecutor shutdown: {task}')
-        return self._loop.run_in_executor(None, task, *args)
+        return self._loop.run_in_executor(self._thread_executor, task, *args)
 
     def shutdown(self, wait: bool = True, cancel_futures: bool = False) -> None:
         """Stop the executor and cancel tasks if needed.
@@ -85,30 +95,43 @@ class AsyncExecutor(Executor):
         :param wait: wait for tasks to be finished or stop immediately
         :param cancel_futures: notify tasks to be cancelled
         """
-        # as said in Executor it may be called several times
         if self._stopped:
             return
 
         self._stopped = True
-        self._loop.stop()
 
         if cancel_futures:
             self.cancel_futures()
 
-        self._submit(self._stop_loop, not wait)
-        del self._loop, self._thread
+        def has_coroutines() -> bool:
+            return bool(self._loop._ready or self._loop._scheduled)
+
+        if wait:
+            while has_coroutines():
+                self._release_gil()
+
+        self._loop.stop()
+
+        while self._loop.is_running():
+            self._release_gil()
+
+        self._loop.close()
+        self._thread.join()
 
     def cancel_futures(self) -> None:
         """Cancel all running tasks."""
-        for task in asyncio.all_tasks(self._loop):
+        for task in self.tasks:
             task.cancel()
+
+    @property
+    def _thread_executor(self) -> ThreadPoolExecutor:
+        return self._loop._default_executor
 
     def _submit(self, task: Callable[..., Awaitable], *args, **kwargs) -> Future:
         coroutine = task(*args, **kwargs)
         return asyncio.run_coroutine_threadsafe(coroutine, self._loop)
 
     @staticmethod
-    async def _stop_loop(force: bool) -> None:
-        """Force an iteration in the loop to meet stop condition."""
-        if force:
-            raise KeyboardInterrupt
+    def _release_gil() -> None:
+        """Release the GIL to let async thread make an iteration."""
+        time.sleep(0)
